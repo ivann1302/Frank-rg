5fb9780c39de919c587e48b661ca7df9c7e756a4
ivan <ivann97n@gmail.com>
2025-11-30 11:00:08 +0300

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  base: process.env.NODE_ENV === 'production' ? '/Frank-rg/' : '/',
   css: {
     preprocessorOptions: {
       scss: {
